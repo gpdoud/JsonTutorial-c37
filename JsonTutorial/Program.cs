8c9c93bd0f362b5a59d7c9aba770a208a20e3e6a
Greg Doud <gdoud@gmail.com>
2022-09-02 14:38:55 -0400

    Add project files.

---
+++ JsonTutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using JsonTutorial;
+
+using System.Text.Json;
+
+User noah = new User {
+    Id = 1, Username = "nphence", Password = "password",
+    Firstname = "Noah", Lastname = "Phence",
+    Phone = "411", Email = "nphence@gmail.com",
+    IsReviewer = true, IsAdmin = false
+};
+
+JsonSerializerOptions options = new JsonSerializerOptions() {
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true
+};
+
+var json = JsonSerializer.Serialize(noah, options);
+
+User? noahConverted = JsonSerializer.Deserialize<User>(json);
+
+var v = 1;
